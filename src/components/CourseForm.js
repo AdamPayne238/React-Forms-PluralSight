c21ccea544d84def060925eae9bc4f057d37c947
Adam Payne <ajpayne238@gmail.com>
2019-09-18 23:22:45 -0400

    Keeping Code DRY and Created Reusable TextInput Component

--- src/components/CourseForm.js
+++ src/components/CourseForm.js
@@ -1,28 +1,49 @@
 import React from "react";
 
+//Numbers Start At TextInput.js where we start to build a reusable Input Component to use instead of individual title, author, and category input forms....lame!
+// #15 import TextInput
+import TextInput from './common/TextInput';
+
 function CourseForm(props) {
   return (
     <form>
-      <div className="form-group">
+      {/* #16 Remove this Code and then add TextInput to <input id="title" etccc
+      
+        <div className="form-group">
         <label htmlFor="title">Title</label>
-        <div className="field">
-          <input
-            id="title"
-            type="text"
-            name="title"
-            className="form-control"
-            value=""
-          />
-        </div>
-      </div>
+        <div className="field"> 
 
+          #16 added TextInput in place of input*/}
+          <TextInput
+            id="title"
+            // #20 Add Label Prop and set to title
+            label="title"
+            // #17 *Remove* type="text"
+            //Add onChange and pass onTitleChange from ManageCoursePage.js
+            //Now onChange
+            onChange={props.onChange}
+            name="title"
+            // #18 *Remove* className="form-control"
+            //set Value to props.course.title from empty string ""
+            value={props.course.title}
+          />
+        {/* #19 Remove Closing Divs 
+        </div>
+      </div> */}
+
+      {/* #21 RINSE AND REPEAT FOR AUTHOR AND CATEGORY */}
       <div className="form-group">
         <label htmlFor="author">Author</label>
         <div className="field">
           <select
             id="author"
             name="authorId"
-            value=""
+            onChange={props.onChange}
+            //set Value to props.course.authorId
+            //The || says if the value is null then set it to an empty string ""
+            //instead of throwing an error on react inspect tools
+            //JS logical OR operator || 
+            value={props.course.authorId || ""}
             className="form-control"
           >
             <option value="" />
@@ -38,9 +59,12 @@ function CourseForm(props) {
           <input
             type="text"
             id="category"
+            //added onCHange to allow typing in catory input field
+            onChange={props.onChange}
             name="category"
             className="form-control"
-            value=""
+            //set Value to props.course.catagory from empty string ""
+            value={props.course.category}
           />
         </div>
       </div>
--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,11 +1,77 @@
-import React from "react";
+import React, {useState} from "react";
+import CourseForm from "./CourseForm";
 
 const ManageCoursePage = props => {
+  //Use array destructuring
+  const [ course, setCourse ] = useState({
+    id: null,
+    slug: "",
+    title: "",
+    authorId: null,
+    catagory: ""
+
+  });
+
+
+  // #1 Way
+  // function handleTitleChange(event){
+
+    //to Avoid Mutating State, lets create a copy of the course object
+      //Create updatedCourse and set it to the spread of course
+
+    // const updatedCourse = {...course, title: event.target.value};
+
+    //   updatedCourse.title = event.target.value; is the same as title: even.target.value
+    // it just allows for more dry code by adding it into the same line as ...course
+    // but seperating with a comma,,,
+
+    //Now we can type in our inputs in title
+    //Still need to add change handlers for author and catagory
+
+  //   setCourse(updatedCourse);
+  // }
+
+  //#2 Explanation
+  //Creating a changeHandler for each input can be daunting. Thankfully, there is a simple pattern for creating a single changeHandler for each form.
+  //This is an example of replicating the above code to get the same input results for the Author Input. Could also copy paste and change to work for catagory.
+  // # 2 Way is jsut a copy of #1 but changed name to fit author input
+  // function handleAuthorChange(event){
+  //   const updatedCourse = {...course, author: event.target.value};
+  //   setCourse(updatedCourse);
+  // }
+
+
+  // #3 Way! The most dry and changed function to work for every input
+  //This way allows us to not create a new handleChange for each input yay!
+
+    //Adding ({ target }) instead of (event) is the same as constt { target } = event;
+    function handleChange({ target }){
+    
+    //Set a property on this object based on the value of this variable.
+    //event.target.name is equal to title so this will end up setting the title property
+    const updatedCourse = {...course, [target.name]: target.value};
+
+    setCourse(updatedCourse);
+  }
+
+  //#4 is more Dificult but the same as #3 just more dry
+  //I hope all these steps help understanding the different and more dry ways to write this code.
+  //#4
+  // function handleChange({ target }){
+  //   setCourse({
+  //     ...course,
+  //     [target.name]: target.value
+  //   });
+  // }
+
   return (
+
     <>
       <h2>Manage Course</h2>
-      {props.match.params.slug}
+      {/* changed onTitleChange to onChange, anddddd changed handleTitleChange to handleChange to allow use with every input and not just title (author and catagory are the other inputs)  */}
+      <CourseForm course={course} onChange={handleChange} />
     </>
+
   );
 };
 
